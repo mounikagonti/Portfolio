f50f47e8303ac189f2158f62cc8120a7db27bab8
mounika gonti <hmounika1997@gmail.com>
2026-04-07 16:13:22 +0530

    Resume

--- modules/about/About.tsx
+++ modules/about/About.tsx
@@ -27,7 +27,7 @@ const About = ({id}: any) => {
             applications.
           </p>
           <a
-            href='/assets/Mounika_Gonti_Resume.pdf'
+            href='/assets/Mounika_Gonti_Frontend_Fresher.pdf'
             target='_blank'
             download={true}
           >
